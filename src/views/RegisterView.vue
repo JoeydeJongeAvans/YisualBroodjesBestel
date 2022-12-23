95a80749fb1dba06559dc06b6932a21c8a838257
JoeydeJonge <jd.dejonge@student.avans.nl>
2022-12-23 16:08:28 +0100

    fix typo

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,7 +3,7 @@
     <div class="navbar">
         <nav class="nav">
             <div class="nav-el" id="name_project">
-                <img src="./assets/logo.png" width="30" class="me-2" alt="" />
+                <img src="../assets/logo.png" width="30" class="me-2" alt="" />
                 Vue Firebase Authentication
             </div>
             <div class="nav-el">
