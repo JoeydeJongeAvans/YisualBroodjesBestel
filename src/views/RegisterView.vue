bc91a8b2b25f3df326d1a547b7d0bca8f6c3a509
JoeydeJonge <jd.dejonge@student.avans.nl>
2023-01-02 14:29:13 +0100

    Add copyright to Register

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -39,6 +39,17 @@
             </form>
         </div>
     </div>
+    <footer>
+    <span class="logo">Joey<br>de Jonge</span>
+    <div class="box">
+      <span class="box__copy">©</span>
+      <div class="box__group">
+        <p class="box__date">2023</p>
+        <hr>
+        <a class="box__link" href="https://github.com/JoeydeJongeAvans/YisualBroodjesBestel">Terms and Conditions</a>
+      </div>
+    </div>
+  </footer>
 </template>
   
 <script>
@@ -87,4 +98,5 @@ export default {
 
 <style>
 @import '../assets/styles/login.css';
+@import '../assets/styles/footer.css';
 </style>
